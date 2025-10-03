b0769a3cacb0f2302fe204d140f3e36b88ce6261
Farnabaz <farnabaz@gmail.com>
2025-10-03 11:26:05 +0200

    fix(content): preserve TOC field on minify (#27)

--- src/app/src/utils/content.ts
+++ src/app/src/utils/content.ts
@@ -146,7 +146,10 @@ async function generateDocumentFromMarkdownContent(id: string, content: string):
   return {
     id,
     meta: {},
-    body,
+    body: {
+      ...body,
+      toc: document.toc,
+    },
     ...document.data,
   } as unknown as DatabaseItem
 }
