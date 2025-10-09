d582bd53149bef88b5ada9c988b4eae25993757f
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-10-09 11:06:17 +0200

    types

--- src/app/src/utils/storage.ts
+++ src/app/src/utils/storage.ts
@@ -1,6 +1,8 @@
 import { createStorage } from 'unstorage'
 import indexedDbDriver from 'unstorage/drivers/indexedb'
 import nullDriver from 'unstorage/drivers/null'
+import type { DraftItem } from '../types/draft'
+import type { DatabaseItem, MediaItem } from '../types'
 
 export const nullStorageDriver = nullDriver()
 
@@ -9,6 +11,6 @@ export const indexedDbStorageDriver = (name: string) => indexedDbDriver({
   storeName: 'drafts',
 })
 
-export const documentStorage = createStorage({ driver: indexedDbStorageDriver('document') })
+export const documentStorage = createStorage <DraftItem<DatabaseItem>>({ driver: indexedDbStorageDriver('document') })
 
-export const mediaStorage = createStorage({ driver: indexedDbStorageDriver('media') })
+export const mediaStorage = createStorage<DraftItem<MediaItem>>({ driver: indexedDbStorageDriver('media') })
