3c6d30275c2165f1a9fa4d756fa81cca5e08e8a7
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-10-28 11:24:39 +0100

    fix(lint): add missing comma

--- src/app/src/composables/useHooks.ts
+++ src/app/src/composables/useHooks.ts
@@ -4,10 +4,10 @@ import { createHooks } from 'hookable'
 export const useHooks = createSharedComposable(() => {
   return createHooks<{
     'studio:draft:document:updated': (
-      { caller, selectItem }: { caller: string, selectItem?: boolean }
+      { caller, selectItem }: { caller: string, selectItem?: boolean },
     ) => void
     'studio:draft:media:updated': (
-      { caller, selectItem }: { caller: string, selectItem?: boolean }
+      { caller, selectItem }: { caller: string, selectItem?: boolean },
     ) => void
   }>()
 })
