2ccc465918d12135396719d7d06cb15ea52ae8fd
Farnabaz <farnabaz@gmail.com>
2025-11-06 16:22:31 +0100

    fix(editor): create portal for monaco overflow widgets

--- src/app/src/composables/useMonaco.ts
+++ src/app/src/composables/useMonaco.ts
@@ -35,12 +35,19 @@ export function useMonaco(target: Ref<HTMLElement | undefined>, options: UseMona
 
     const colorMode = unref(options.colorMode) || 'dark'
 
+    const monacoPortal = (el.getRootNode() as Document)?.getElementById('monaco-portal') || undefined
+    if (monacoPortal) {
+      monacoPortal.style.position = 'absolute'
+      monacoPortal.style.top = el.getClientRects()[0].top + 'px'
+    }
+
     // Create editor instance (use the custom createEditor wrapper from setupMonaco)
     editor.value = monaco.createEditor(el, {
       theme: getTheme(colorMode),
       lineNumbers: 'off',
       readOnly: options.readOnly ?? false,
       wordWrap: 'on',
+      overflowWidgetsDomNode: monacoPortal,
       scrollbar: options.readOnly
         ? {
             vertical: 'hidden',
