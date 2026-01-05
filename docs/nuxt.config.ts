c48011b7f7fd1ab9870b08c8e0fc2a591931e1cf
Baptiste Leproux <leproux.baptiste@gmail.com>
2026-01-05 17:55:07 +0100

    docs(config): add llms

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,6 +2,15 @@ export default defineNuxtConfig({
   extends: ['docus'],
   modules: ['../src/module/src/module', '@nuxtjs/plausible'],
   css: ['~/assets/css/main.css'],
+  llms: {
+    domain: 'https://nuxt.studio',
+    title: 'Nuxt Studio',
+    description: 'Edit your Nuxt Content website in production.',
+    full: {
+      title: 'Nuxt Studio',
+      description: 'Edit your Nuxt Content website in production.',
+    },
+  },
   studio: {
     repository: {
       provider: 'github',
