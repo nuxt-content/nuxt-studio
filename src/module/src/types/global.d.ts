ab066099b224d07df521564eaa02d3e9fc68264f
Benjamin Oddou <85166574+BenjaminOddou@users.noreply.github.com>
2026-01-14 12:27:23 +0100

    feat(auth): add login and logout server runtime hooks (#263)
    Co-authored-by: Baptiste Leproux <leproux.baptiste@gmail.com>

--- src/module/src/types/global.d.ts
+++ src/module/src/types/global.d.ts
@@ -11,3 +11,13 @@ declare module '#build/studio-public-assets' {
 
   export const publicAssetsStorage: Storage
 }
+
+declare module 'nitropack' {
+  import type { StudioUser } from 'nuxt-studio/app'
+  import type { H3Event } from 'h3'
+
+  interface NitroRuntimeHooks {
+    'studio:auth:login': (payload: { user: StudioUser, event: H3Event }) => void
+    'studio:auth:logout': (payload: { user: StudioUser, event: H3Event }) => void
+  }
+}
