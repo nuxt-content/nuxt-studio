0ed82ad9561d2e12f21666204e62b9ebbe87b52c
Baptiste Leproux <leproux.baptiste@gmail.com>
2026-02-09 12:31:44 +0100

    fix(app): do not expose variable from app

--- src/app/src/shared.ts
+++ src/app/src/shared.ts
@@ -3,7 +3,6 @@ export type { MarkdownParsingOptions } from './types/content'
 export type { GitProviderType } from './types/git'
 export type { ComponentMeta } from './types/component'
 export type { AIGenerateOptions, AIHintOptions, CursorContext, DiffPart, AITransformCallbacks } from './types/ai'
-export { AI_LIMITS } from './types/ai'
 
 // Temporary export for remark emoji plugin
 export * from './utils/emoji'
